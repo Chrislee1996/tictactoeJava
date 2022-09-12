f0ca9bb2d2dc5fe0c71f60c015ee7e8b7e7b6910
chrislee1996 <prepchrislee@gmail.com>
2022-09-12 13:23:58 -0400

    Added switch and case movements so player can now enter moves and will update the board

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,39 @@ public class Main {
 
         System.out.println(userInput);
 
+        switch(userInput) {
+            case "1":
+                board[0][0] = 'X';
+                break;
+            case "2":
+                board[0][1] = 'X';
+                break;
+            case "3":
+                board[0][2] = 'X';
+                break;
+            case "4":
+                board[1][0] = 'X';
+                break;
+            case "5":
+                board[1][1] = 'X';
+                break;
+            case "6":
+                board[1][2] = 'X';
+                break;
+            case "7":
+                board[2][0] = 'X';
+                break;
+            case "8":
+                board[2][1] = 'X';
+                break;
+            case "9":
+                board[2][2] = 'X';
+                break;
+            default:
+                System.out.println("Invalid move :(");
+        }
+
+        printBoard(board);
     }
 
 
