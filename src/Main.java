681ed138bdd3930b3cf227e15abd46bf8243959a
chrislee1996 <prepchrislee@gmail.com>
2022-09-12 12:49:55 -0400

    created separate method for board

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         printBoard(board);
     }
 
+
     private static void printBoard(char[][] board) {
         System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
         System.out.println("-+-+-");
